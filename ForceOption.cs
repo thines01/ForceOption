a78bf35741f090b9893238ce55b4cdc5463f00e6
thines01 <thines01@yahoo.com>
2023-07-08 16:15:29 -0500

    Add project files.

---
+++ ForceOption.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace ForceOption
+{
+   public class CForceOption
+   {
+      private string[] _args { get; set; }
+      private static string[] arr_strOptions = { "/f", "-f", "/force", "-force" };
+
+      public CForceOption(string[] args) { _args = args; }
+
+      public bool isForced { get { return IsForced(_args); } private set { } }
+
+      public static bool IsForced(string[] args)
+      {
+         return
+            args.Any(strArg =>
+               arr_strOptions.Any(strOpt =>
+                  strOpt.Equals(strArg, System.StringComparison.CurrentCultureIgnoreCase)));
+      }
+   }
+}
